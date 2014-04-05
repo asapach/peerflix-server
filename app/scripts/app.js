b106cba8bf5df2d38a7d9b5df56faada31e66556
Aliaksei Sapach <aliaksei.dreamsonic@gmail.com>
2014-04-05 14:25:02 +0300

    added yeoman angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('peerflixServerApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
