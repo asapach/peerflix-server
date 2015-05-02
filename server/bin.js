232fa8d48bb1a35bdbb985cd5ccb46c51ce0051c
Aliaksei Sapach <dreamsonic@tut.by>
2015-05-02 18:57:46 +0300

    set maxage on static resources

--- server/bin.js
+++ server/bin.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
+var STATIC_OPTIONS = { maxAge: 3600000 };
+
 var express = require('express'),
   http = require('http'),
   path = require('path'),
   socket = require('./socket'),
   api = require('./')
-    .use(express.static(path.join(__dirname, '../dist')))
-    .use(express.static(path.join(__dirname, '../.tmp')))
-    .use(express.static(path.join(__dirname, '../app')));
+    .use(express.static(path.join(__dirname, '../dist'), STATIC_OPTIONS))
+    .use(express.static(path.join(__dirname, '../.tmp'), STATIC_OPTIONS))
+    .use(express.static(path.join(__dirname, '../app'), STATIC_OPTIONS));
 
 var server = http.createServer(api);
 socket(server);
